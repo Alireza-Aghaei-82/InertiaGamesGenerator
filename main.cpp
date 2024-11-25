be73a8f3c95e0b2fa2b5696110e8f5870b9e65af
Alireza Aghaei <alireza.aghae.82@gmail.com>
2024-11-25 21:53:41 +0330

    InertiaGamesGenerator 1.0

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <QApplication>
+#include <QQmlApplicationEngine>
+
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.addImportPath("G:/programs/Qt/6.8.0/mingw_64/custom_plugins");
+
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+
+    engine.loadFromModule("InertiaGamesGenerator", "Main");
+
+    return app.exec();
+}
